0bbf420c448d9ce4ded6c150fa581c057ecf4d43
Walker Davis <walkerreeddavis@gmail.com>
2025-10-29 13:45:19 -0500

    updated Starter file with loop

--- Grade Calculator/src/main/java/Starter.java
+++ Grade Calculator/src/main/java/Starter.java
@@ -2,22 +2,33 @@ import java.util.Scanner;
 
 public class Starter {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         IO.println("Hello World");
 
         Scanner scan = new Scanner(System.in);
-        double grade;
+        double grade = 0;
         GradeConverter gc;
+        boolean invalidInput = true;
 
-        IO.println("Enter your number grade: ");
-        try {
-            grade = scan.nextDouble();
-            gc = new GradeConverter(grade);
-            gc.convert();
-        } catch (Exception e){
-            System.out.println("Not a number. Try again!");
+        while (invalidInput){
+            try {
+                IO.println("Enter value between 0 and 100: ");
+                grade = scan.nextDouble();
+                if ((grade <= 100) && (grade >= 0)){
+                    invalidInput = false;
+                } else {
+                    IO.println("Value out of range. Try again!");
+                }
+            } catch (Exception e) {
+                System.out.println("Not a number. Try again!");
+                scan.nextLine();
+            }
+
         }
+        gc = new GradeConverter(grade);
+        gc.convert();
+        scan.close();
 
     }
-
 }
+

5cec355aa13b987f89857248f513e5e4e746517c
Walker Davis <walkerreeddavis@gmail.com>
2025-10-29 12:45:25 -0500

    committing grade converter project

---
+++ src/main/java/Starter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Starter {
+
+    public static void main(String[] args){
+        IO.println("Hello World");
+
+        Scanner scan = new Scanner(System.in);
+        double grade;
+        GradeConverter gc;
+
+        IO.println("Enter your number grade: ");
+        try {
+            grade = scan.nextDouble();
+            gc = new GradeConverter(grade);
+            gc.convert();
+        } catch (Exception e){
+            System.out.println("Not a number. Try again!");
+        }
+
+    }
+
+}
